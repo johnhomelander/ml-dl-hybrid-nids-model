4a9cb12cd0a63346bb33b16b71e286212df69ec7
johnhomelander <sahnipranav10@gmail.com>
2025-04-09 19:02:51 +0530

    first test with collected data, feature mapping done

--- app/app.py
+++ app/app.py
@@ -10,6 +10,86 @@ class_name={0:'BENIGN',7: 'FTP-Patator',11: 'SSH-Patator',1: 'Bot',10: 'PortScan
  'Web Attack � Sql Injection',6: 'DoS slowloris',5:'DoS Slowhttptest'
  ,4:'DoS Hulk',3: 'DoS GoldenEye',8: 'Heartbleed',2: 'DDoS',9: 'Infiltration'}
 
+feature_mapping = {
+   "Dst Port":" Destination Port",
+    "Flow Duration":" Flow Duration",
+    "Total Fwd Packet":" Total Fwd Packets",
+    "Total Bwd packets":" Total Backward Packets",
+    "Total Length of Fwd Packet":"Total Length of Fwd Packets",
+    "Total Length of Bwd Packet":" Total Length of Bwd Packets",
+    "Fwd Packet Length Max":" Fwd Packet Length Max",
+    "Fwd Packet Length Min":" Fwd Packet Length Min",
+    "Fwd Packet Length Mean":" Fwd Packet Length Mean",
+    "Fwd Packet Length Std":" Fwd Packet Length Std",
+    "Bwd Packet Length Max":"Bwd Packet Length Max",
+    "Bwd Packet Length Min":" Bwd Packet Length Min",
+    "Bwd Packet Length Mean":" Bwd Packet Length Mean",
+    "Bwd Packet Length Std":" Bwd Packet Length Std",
+    "Flow Bytes/s":"Flow Bytes/s",
+    "Flow Packets/s":" Flow Packets/s",
+    "Flow IAT Mean":" Flow IAT Mean",
+    "Flow IAT Std":" Flow IAT Std",
+    "Flow IAT Max":" Flow IAT Max",
+    "Flow IAT Min":" Flow IAT Min",
+    "Fwd IAT Total":"Fwd IAT Total",
+    "Fwd IAT Mean":" Fwd IAT Mean",
+    "Fwd IAT Std":" Fwd IAT Std",
+    "Fwd IAT Max":" Fwd IAT Max",
+    "Fwd IAT Min":" Fwd IAT Min",
+    "Bwd IAT Total":"Bwd IAT Total",
+    "Bwd IAT Mean":" Bwd IAT Mean",
+    "Bwd IAT Std":" Bwd IAT Std",
+    "Bwd IAT Max":" Bwd IAT Max",
+    "Bwd IAT Min":" Bwd IAT Min",
+    "Fwd PSH Flags":"Fwd PSH Flags",
+    "Bwd PSH Flags":" Bwd PSH Flags",
+    "Fwd URG Flags":" Fwd URG Flags",
+    "Bwd URG Flags":" Bwd URG Flags",
+    "Fwd Header Length":" Fwd Header Length",
+    "Bwd Header Length":" Bwd Header Length",
+    "Fwd Packets/s":"Fwd Packets/s",
+    "Bwd Packets/s":" Bwd Packets/s",
+    "Packet Length Min":" Min Packet Length",
+    "Packet Length Max":" Max Packet Length",
+    "Packet Length Mean":" Packet Length Mean",
+    "Packet Length Std":" Packet Length Std",
+    "Packet Length Variance":" Packet Length Variance",
+    "FIN Flag Count":"FIN Flag Count",
+    "SYN Flag Count":" SYN Flag Count",
+    "RST Flag Count":" RST Flag Count",
+    "PSH Flag Count":" PSH Flag Count",
+    "ACK Flag Count":" ACK Flag Count",
+    "URG Flag Count":" URG Flag Count",
+    "CWR Flag Count":" CWE Flag Count",
+    "ECE Flag Count":" ECE Flag Count",
+    "Down/Up Ratio":" Down/Up Ratio",
+    "Average Packet Size":" Average Packet Size",
+    "Fwd Segment Size Avg":" Avg Fwd Segment Size",
+    "Bwd Segment Size Avg":" Avg Bwd Segment Size",
+    "Fwd Header Length.1":" Fwd Header Length.1",
+    "Fwd Bytes/Bulk Avg":"Fwd Avg Bytes/Bulk",
+    "Fwd Packet/Bulk Avg":" Fwd Avg Packets/Bulk",
+    "Fwd Bulk Rate Avg":" Fwd Avg Bulk Rate",
+    "Bwd Bytes/Bulk Avg":" Bwd Avg Bytes/Bulk",
+    "Bwd Packet/Bulk Avg":" Bwd Avg Packets/Bulk",
+    "Bwd Bulk Rate Avg":"Bwd Avg Bulk Rate",
+    "Subflow Fwd Packets":"Subflow Fwd Packets",
+    "Subflow Fwd Bytes":" Subflow Fwd Bytes",
+    "Subflow Bwd Packets":" Subflow Bwd Packets",
+    "Subflow Bwd Bytes":" Subflow Bwd Bytes",
+    "FWD Init Win Bytes":"Init_Win_bytes_forward",
+    "Bwd Init Win Bytes":" Init_Win_bytes_backward",
+    "Fwd Act Data Pkts":" act_data_pkt_fwd",
+    "Fwd Seg Size Min":" min_seg_size_forward",
+    "Active Mean":"Active Mean",
+    "Active Std":" Active Std",
+    "Active Max":" Active Max",
+    "Active Min":" Active Min",
+    "Idle Mean":"Idle Mean",
+    "Idle Std":" Idle Std",
+    "Idle Max":" Idle Max",
+    "Idle Min":" Idle Min"}
+
 app=Flask('ml-dl-hybrid-model')
 
 clf_xgb = joblib.load('../xgb_model.pkl')
@@ -27,8 +107,8 @@ def preprocess_input(df):
     df_selected=df[selected_features]
     arr = df_selected.values
 
-    features_ml = arr.reshape(1,-1)
-    features_lstm = arr.reshape(1,1,-1)
+    features_ml = arr
+    features_lstm = arr.reshape(arr.shape[0],1,arr.shape[1])
 
     X_mi=selector_mi.transform(features_ml)
     X_mi_scaled = scaler_pca.transform(X_mi)
@@ -43,7 +123,10 @@ def predict():
     try:
         data = request.get_json()
         features = pd.DataFrame(data)
+        print(features)
+        print(features.shape)
 
+        features.rename(columns=feature_mapping, inplace=True)
         X_final,features_lstm = preprocess_input(features)
 
         xgb_prediction = clf_xgb.predict(X_final)
@@ -52,6 +135,7 @@ def predict():
         threshold = 0.5
         max_prob = np.max(xgb_probs)
 
+        print(X_final)
         if max_prob < threshold:
             prediction = lstm_model.predict(features_lstm)
             predicted_class = int(np.argmax(prediction, axis=1)[0])
--- app/test-script.py
+++ app/test-script.py
@@ -1,27 +1,37 @@
 import pandas as pd
 import numpy as np
 import json,requests
+import time
 
 df=pd.read_csv('../testing/2025-04-08_Flow.csv')
-columns_to_remove = ['Timestamp', 'Flow ID', 'Source IP', 'Destination IP', 'Protocol']
+columns_to_remove = ['Timestamp', 'Flow ID', 'Src IP', 'Dst IP', 'Protocol','Src Port','Label']
 df = df.drop(columns=[col for col in columns_to_remove if col in df.columns])
 url='http://127.0.0.1:5000/predict'
 
-print(df.columns)
-#for idx, row in df.iterrows():
-#    data = row.to_frame().T.to_dict(orient='records')  # One record per row
-#
-#    try:
-#        response = requests.post(url, json=data)
-#        result = response.json()
-#        print(result)
-#
-#
-#        # Optional delay to mimic real-time streaming
-#        time.sleep(0.5)
-#
-#    except Exception as e:
-#        print(f"Error with record #{idx + 1}: {e}")
+#print(df['Fwd Header Length'])
+df['Fwd Header Length.1'] = df['Fwd Header Length']
+print(df[['Fwd Header Length','Fwd Header Length.1']])
+print(df.columns,len(df.columns))
+
+batch_size = 100
+for idx, row in df.iterrows():
+    data = row.to_frame().T.to_dict(orient='records')  # One record per row
+
+#for i in range(0, len(df), batch_size):
+#    batch = df.iloc[i:i + batch_size]
+#    data = batch.to_dict(orient='records')
+    #print(data)
+    try:
+        response = requests.post(url, json=data)
+        result = response.json()
+        print(result)
+
+
+        # Optional delay to mimic real-time streaming
+        #time.sleep(0.5)
+
+    except Exception as e:
+        print(f"Error with record #{idx + 1}: {e}")
 #for i in range(1909):
 #    print(dts[i])
 #    payload = {
